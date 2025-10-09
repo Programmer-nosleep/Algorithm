c2e6a5199b96fc826f9cb62457891f8424739341
ahmadzanisy <ahmadzanisy@gmail.com>
2025-10-09 22:33:37 +0700

    update

---
+++ basic/leetcode/cpp/MiddleLinkedList/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <array>
+
+void array_list()
+{
+  std::array<int, 10> arr_list { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+}
+
+int main (int argc, char *argv[]) 
+{
+  return 0;
+}
